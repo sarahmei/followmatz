fb0f47949f249573ca4e16f7b777079ed5f79cba
Sarah Mei <sarahmei@gmail.com>
2010-06-10 19:23:01 -0700

    Refactored javascript and status updates controller.

--- app/controllers/status_updates_controller.rb
+++ app/controllers/status_updates_controller.rb
@@ -1,19 +1,17 @@
 class StatusUpdatesController < ApplicationController
+  before_filter :get_update, :only => [:edit, :edit_notes, :update, :update_notes, :add_vote]
 
   def edit
-    @status_update = StatusUpdate.find(params[:id])
     render :partial => 'edit_translation'
   end
 
   def edit_notes
-    @status_update = StatusUpdate.find(params[:id])
     note_rows = @status_update.translation_notes.split(/$/).length rescue 0
     @textarea_rows = [note_rows + 1, 3].max
     render :partial => 'edit_notes'
   end
 
   def update
-    @status_update = StatusUpdate.find(params[:id])
     if @status_update.update_attributes(params[:status_update])
       flash[:notice] = 'Translation successfully updated.'
     end
@@ -21,7 +19,6 @@ class StatusUpdatesController < ApplicationController
   end
 
   def update_notes
-    @status_update = StatusUpdate.find(params[:id])
     if @status_update.update_attributes(params[:status_update])
       flash[:notice] = 'Notes successfully updated.'
     end
@@ -29,7 +26,6 @@ class StatusUpdatesController < ApplicationController
   end
 
   def add_vote
-    @status_update = StatusUpdate.find(params[:id])
     @status_update.votes.build(:vote => true)
     @status_update.save
     render :partial => 'thumbs_up', :locals => {:status_update => @status_update, :enable_voting => false}
@@ -37,15 +33,21 @@ class StatusUpdatesController < ApplicationController
 
   def interesting
     @updates = StatusUpdate.paginate(:all, :page => params[:page], :per_page => 10,
-      :include => :votes, :conditions => ['votes.vote = ?', true]).sort do |a,b|
+    :include => :votes, :conditions => ['votes.vote = ?', true]).sort do |a,b|
       b.votes.length <=> a.votes.length
     end
   end
 
   def ruby_related
     @updates = StatusUpdate.paginate(:all, :page => params[:page], :per_page => 10,
-                                            :conditions => "original_text like '%Ruby%'",
-                                            :order => "twitter_date DESC" )
+    :conditions => "original_text like '%Ruby%'",
+    :order => "twitter_date DESC")
+  end
+
+  private
+
+  def get_update
+    @status_update = StatusUpdate.find(params[:id])
   end
 
 end
